55a699ea33452c3a1173635eaf308a1071a1a913
JoschkaZ <Joschka.Ziesche15@imperial.ac.uk>
2018-06-19 10:58:24 +0200

    .

---
+++ Recipes #1.py
@@ -0,0 +1,6 @@
+from sklearn import tree
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
+clf = tree.DecisionTreeClassifier() #classifier is box of rules
+clf = clf.fit(features, labels) #training algorithms creates rules
+print(clf.predict([[150, 0]]))
